ff8b5b89d7b4e82c66f8ca9e632f95399d86e014
Hannah Pinson <pinsonhannah@gmail.com>
2014-12-07 20:43:49 +0100

    refactored, added exports for client-side validation, updated tests.

--- server/modules/validation.js
+++ server/modules/validation.js
@@ -1,83 +1,56 @@
 
-/*** @HANNAH *** 
-	Todo's in this code:
-	   	>> Use of global variable re (the JS-trap!)
-	   	>> Lots of code duplication, basically always the same pattern
-	   	>> Cleaner example:
+/**
+* Module for validation of input with regular expressions, for client- and server-side validation.
+* @module validation
+*/
 
-	// higher order function to generate check functions
 
-	function check(regex) {
-		return function(inp) {
-			return regex.test(inp);
+/*
+* TO DO
+* - validation of research domain (check against predefined list of possible domains)
+* - validation of language preference
+*/
+
+
+//**** definition of regular expressions, exported for use with AngularJS (client-side validation)
+
+
+//all regex imply that the matched string should not be empty (=solely whitespace characters) and should not contain leading or trailing whitespaces
+
+// only letters, numbers and underscores
+var RegEx_username = /^\w+$/; 
+
+// common email 
+var RegEx_emailAdress = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+//at least one number, at least 6 and at most 20 characters from the set [a-zA-Z0-9!@#$%^&*]
+var RegEx_password = /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,20}$/; 
+
+//one or more words in all languages, with apostrophes and hyphens 
+//excludes numbers and all the special (non-letter) characters commonly found on keyboards
+var RegEx_generalName = /^[a-zA-Z\xC0-\uFFFF '-]+[a-zA-Z\xC0-\uFFFF'-]$/; 
+
+
+exports.RegEx_username = RegEx_username;
+exports.RegEx_emailAdress = RegEx_emailAdress;
+exports.RegEx_password = RegEx_password;
+exports.RegEx_generalName = RegEx_generalName;
+
+
+
+//**** functions used for server-side validation 
+
+// includes testing if input is defined
+
+function check(regex) {
+		return function(input) {
+			return input && regex.test(input); 
 		}
-	}
+}
 
-	// followed by these definitions
-
-	var nonEmpty = check(/.\$+./);
-	var isPassword = check(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/);
-	var isUsername = check(/^\w+$/);
-	var isEmailAdress = ...
-	...
-
-	// OR YOU CAN EVEN DEFINE CHECK LIKE THIS ...
-	// ... TO ELIMINATE NEEDED_PROPERTIES-CHECK IN USER.JS
-
-	function check(regex) {
-		return function(inp) {
-			return inp && regex.test(inp);
-		}
-	}
-
-	// ... MAYBE ALSO ADD !isEmpty etc. to check?
-
-/*** --- END --- ***/ 
-
-exports.nonEmpty = function(input) {
-	re = /.\S+./; // RE for any number of characters (.), followed by at least one non-whitespace character (\S)
-	return (re.test(input));
-};
-
-exports.noLeadingWhitespace = function(input) {
-	re = /(\s+[\S\s]*)/; //RE for one or more (+) whitespace characters (\s) before any other character ([\S\s]*)
-	return !(re.test(input));
-};
-
-exports.noTrailingWhitespace = function(input) {
-	re = /([\S\s]*\s+)/; //RE for one or more (+) whitespace characters (\s) after any other character ([\S\s]*)
-	return !(re.test(input));
-};
+exports.isEmailAdress = check(RegEx_EmailAdress);
+exports.isPassword = check(RegEx_Password); 
+exports.isUsername = check(RegEx_username); 
+exports.isGeneralName = check(RegEx_GeneralName);
 
 
-
-//!= RFC2822 standard regex
-exports.isEmailAdress = function(input) { 
-	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(input);
-};
-
-exports.isPassword = function(input) {
-	//at least one number, one lowercase and one uppercase letter 
-	// at least six characters
-	var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-	return re.test(input);
-};
-
-exports.isUsername = function(input) {
-	// only letters, numbers and underscores
-	var re = /^\w+$/; 
-	return re.test(input);
-};
-
-exports.isGeneralName = function(input){
-	//one or more words in all languages, with apostrophes and hyphens 
-	//excludes numbers and all the special (non-letter) characters commonly found on keyboards
-	var re = /^[a-zA-Z\xC0-\uFFFF '-]+[a-zA-Z\xC0-\uFFFF'-]*/;
-	return re.test(input);
-};
-
-exports.isDefinedResearchField = function(input){
-	// ... file with list of research fields needed
-	return true;
-};
