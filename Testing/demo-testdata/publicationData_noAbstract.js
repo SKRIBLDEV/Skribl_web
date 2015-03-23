a08d053240b6a2e36058281b7b6733bbc6b8ddb2
Hannah Pinson <pinsonhannah@gmail.com>
2015-03-23 21:04:40 +0100

    Test data collection (some members of SOFT and some of their publications)

---
+++ Testing/demo-testdata/publicationData_noAbstract.js
@@ -0,0 +1,102 @@
+module.exports = [
+{ ID: 1,
+  title: "Orchestrating nomadic mashups using workflows",
+  authors: 
+   [ { firstName: "Eline", lastName: "Philips" },
+     { firstName: "Andoni Lombide", lastName: "Carreton" },
+     { firstName: "Niels", lastName: "Jonckheere" },
+     { firstName: "Viviane", lastName: "Jonckers" },
+     { firstName: "Wolfgang", lastName: "De Meuter" } ],
+  type : "proceeding",
+  bookTitle: "Proceedings of the 3rd and 4th International Workshop on Web APIs and Services Mashups",
+  organization :"ACM",
+  year: "2010",
+  citations: 3,
+  article_url: "http://soft.vub.ac.be/~njonchee/publications/Mashups2009.pdf" },
+
+
+{ ID: 2,
+  title: "Automatic parallelization of side-effecting higher-order scheme programs",
+  authors: 
+   [ { firstName: "Jens", lastName: "Nicolay" },
+     { firstName: "Coen", lastName: "De Roover" },
+     { firstName: "Viviane", lastName: "Jonckers" },
+     { firstName: "Wolfgang", lastName: "De Meuter" } ],
+  type : "proceeding",
+  bookTitle: "Source Code Analysis and Manipulation (SCAM), 2011 11th IEEE International Working Conference on",
+  organization :"IEEE",
+  year: "2011",
+  citations: 1,
+  article_url: "http://soft.vub.ac.be/~njonchee/publications/Mashups2009.pdf" },
+
+
+
+{ ID: 3,
+  title: "A formal approach to model refactoring and model refinement",
+  authors: 
+   [ { firstName: "Ragnhild", lastName: "Van Der Straeten" },
+     { firstName: "Viviane", lastName: "Jonckers" },
+     { firstName: "Tom", lastName: "Mens" }],
+  type : "journal",
+  journal : "Software \& Systems Modeling",
+  publisher :"Springer",
+  year: "2007",
+  citations: 45,
+  article_url: "http://people.irisa.fr/Jean-Marc.Jezequel/enseignement/M2RI/MDE/Articles/VanDerStaeten07.pdf" },
+
+{ ID: 4,
+  title: "Supporting model refactorings through behaviour inheritance consistencies",
+  authors: 
+   [ { firstName: "Ragnhild", lastName: "Van Der Straeten" },
+     { firstName: "Viviane", lastName: "Jonckers" },
+     { firstName: "Tom", lastName: "Mens" }],
+  type : "proceeding",
+  bookTitle : "UML 2004—The Unified Modeling Language. Modeling Languages and Applications",
+  publisher :"Springer",
+  year: "2004",
+  citations: 41,
+  article_url: "http://informatique.umons.ac.be/ftp_infofs/2004/UML2004-ModelRefactoring.pdf" },
+
+
+{ ID: 5,
+  title: "Incremental resolution of model inconsistencies",
+  authors: 
+   [ { firstName: "Ragnhild", lastName: "Van Der Straeten" },
+     { firstName: "Tom", lastName: "Mens" }],
+  type : "proceeding",
+  bookTitle : "Recent Trends in Algebraic Development Techniques",
+  publisher :"Springer",
+  year: "2007",
+  citations: 30,
+  article_url: "http://www.cs.toronto.edu/~jsimmond/docs/TomMensEtAl.pdf" },
+
+
+{ ID: 6,
+  title: "Determining dynamic coupling in JavaScript using object type inference",
+  authors: 
+   [ { firstName: "Jens", lastName: "Nicolay" },
+    { firstName: "Coen", lastName: "De Roover" },
+    { firstName: "Wolgang", lastName: "De Meuter" },
+     { firstName: "Carlos", lastName: "Noguara" }],
+  type : "proceeding",
+  bookTitle : "Source Code Analysis and Manipulation (SCAM), 2013 IEEE 13th International Working Conference on",
+  publisher :"IEEE",
+  year: "2013",
+  citations: 1,
+  article_url: "ftp://prog.vub.ac.be/tech_report/2013/vub-soft-tr-13-07.pdf" },
+
+{ ID: 7,
+  title: "Verification of business process quality constraints based on visual process patterns",
+  authors: 
+   [ { firstName: "Alexander", lastName: "Forster" },
+     { firstName: "Gregor", lastName: "Engels" },
+     { firstName: "Tim", lastName: "Schattkowsky" },
+     { firstName: "Ragnhild", lastName: "Van Der Straeten" } ],
+  type : "proceeding",
+  bookTitle: "Theoretical Aspects of Software Engineering, 2007. TASE07. First Joint IEEE/IFIP Symposium on",
+  organization :"IEEE",
+  year: "2007",
+  citations: 69,
+  article_url: "http://is.uni-paderborn.de/uploads/tx_sibibtex/Verification_of_Business_Process_Quality_Constraints_Based_on_Visual_Process_Patterns.pdf" }
+
+]
---
+++ Testing/demo-testdata/userData.js
@@ -0,0 +1,7 @@
+var userArray = [
+{firstName:'Ragnhild', lastName:'Van Der Straeten', username:'RvdStraeten', password:'keeF5gee5', email:'rvdr@vub.ac.be', language:'ENG', institution: 'Vrije Universiteit Brussel', faculty: 'Faculty of Science and Bio-engineering Sciences', department: 'Computer Science', researchGroup: 'Software Languages Lab', researchDomains: ['Computer and Information Science', 'Software Engineering']},
+{firstName:'Jens', lastName:'Nicolay', username:'JNicolay', password:'keeF5gee5', email:'jn@vub.ac.be', language:'ENG', institution: 'Vrije Universiteit Brussel', faculty: 'Faculty of Science and Bio-engineering Sciences', department: 'Computer Science', researchGroup: 'Software Languages Lab', researchDomains: ['Computer and Information Science', 'Software Engineering']},
+{firstName:'Wolfgang', lastName:'De Meuter', username:'WDMeuter', password:'keeF5gee5', email:'wdm@vub.ac.be', language:'ENG', institution: 'Vrije Universiteit Brussel', faculty: 'Faculty of Science and Bio-engineering Sciences', department: 'Computer Science', researchGroup: 'Software Languages Lab', researchDomains: ['Computer and Information Science', 'Programming Languages']}
+]
+
+
