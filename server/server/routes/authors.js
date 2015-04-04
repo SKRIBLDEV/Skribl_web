9493561e7706dfff40dd4c0505e10f163ed3342a
noahvanes <noahvanes@me.com>
2015-04-04 22:32:29 +0200

    moved queryAdvanced to a POST-request for compability reasons

--- server/server/routes/authors.js
+++ server/server/routes/authors.js
@@ -30,8 +30,6 @@ function searchAuthors(req, res, context) {
 		if(err)
 			serverError(res, err.toString());
 		else {
-			console.log('results for: ' + firstname + ' & ' + lastname + 'with limit ' + limit);
-			console.log(authors);
 			res.status(200);
 			res.json(authors);
 		}
